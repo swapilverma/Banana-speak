7cd54b0164fecd2e6f95459be48c1cc8fd565ba0
Swapil Verma <swapilverma23@gmail.com>
2021-05-17 17:36:06 +0530

    js imported

---
+++ app.js
@@ -0,0 +1,11 @@
+// Not an ideal way to show output
+
+//input
+var username = prompt("Give me your username")
+
+// processing 
+var welcomeMessage = "this script works! " + username;
+
+// output 
+alert(welcomeMessage)
+
